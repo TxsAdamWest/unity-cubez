de1c5687b5934d15c647962f0afb56921e898e76
Adan <twinadanz@gmail.com>
2018-06-20 21:32:10 -0500

    add constant force physics and regulate framerate / performance to player 1

---
+++ Assets/Movement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	
+	// Update is called once per frame
+	void FixedUpdate () { // FixedUpdate is better for physics based functions.
+
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+
+	}
+}
