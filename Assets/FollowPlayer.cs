49534682fdd6c04cfe3305f5f101b0ae56ffd173
Adan <twinadanz@gmail.com>
2018-06-28 22:37:21 -0500

    Add follow camera to player

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset; // Stores 3 floats.  Perfect for 3-d positioning.
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.position + offset;
+	}
+}
