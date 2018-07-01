93a977e2371c299ff103f0c67be03ce03bfa7775
Adan <twinadanz@gmail.com>
2018-06-30 21:53:17 -0500

    added player collision

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public Movement movement;
+
+    void OnCollisionEnter(Collision collisionInfo) // This built in Unity function will run anytime a rigid body object collides with another.
+    {
+        if (collisionInfo.collider.tag == "Obstacle") // Checking for tags is much more efficient than checking for names of every object in a scene. 
+        {
+            movement.enabled = false;
+        }
+    }
+}
